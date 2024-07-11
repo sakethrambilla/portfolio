e86588df3bc73eb08d78ce29c0c389eb91128387
sakethrambilla <billasaketh@gmail.com>
2024-07-11 21:56:27 +0530

    updated homepage header v.1.3.0

--- app/(home)/components/Header.tsx
+++ app/(home)/components/Header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
   return (
     <div className="relative flex h-[100vh] min-h-[100vh] w-full flex-col items-center justify-start">
       <div className="flex h-[80%] w-full flex-col items-center justify-center gap-8 p-4 text-center">
-        <h1 className="w-ful w-2/3 font-redHatDisplay text-xl text-white lg:text-3xl">
-          Hi 👋, my name is Saketh Ram Billa and I am a
+        <h1 className="w-full text-left font-redHatDisplay text-xl leading-[1.0] text-white lg:text-[4rem] xl:text-[8rem]">
+          Transforming Ideas into Code.
+          <br />{" "}
+          <span className="font-cormorant_garamond text-primary lg:text-[5rem] xl:text-[10rem]">
+            One Line of Code at a Time
+          </span>
         </h1>
-        <h3 className="font-cormorant_garamond flex flex-col items-center text-[3rem] text-primary lg:flex-row lg:gap-12 xl:text-[6rem] 2xl:text-[7rem]">
-          Web Developer
-          <span className="text-white">{"&"}</span> ML Engineer
-        </h3>
       </div>
     </div>
   );
